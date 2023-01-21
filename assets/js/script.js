94e4f869d8a947c75c772349558796e8e2ce5f97
Stefan Medves <stefan.medves@gmail.com>
2023-01-21 00:39:31 +0000

    Add Modal content and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 let copywright = document.getElementById("copywright");
 let year = new Date().getFullYear();
+const modal = document.getElementById("myModal");
+// button that opens the modal
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+// open the modal on button click
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// close the modal when clicking the X
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// close the modal if user clicks anywhere outside the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 copywright.innerHTML=(year)
